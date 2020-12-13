8dc6ff66c73c903b48f6c14353edb5c222dc79b3
ryfi <6692977+ryfi@users.noreply.github.com>
2020-12-12 22:48:48 -0800

    Naming updated

--- docker/chiron-squeezemeta/build.sh
+++ docker/chiron-squeezemeta/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# This is a template only at the moment. chiron-squeezemeta does not exist at the moment.
 
 IMAGE_VERSION='1.0.0'
 
-docker rmi -f umigs/chiron-humann2
-docker build --no-cache -t umigs/chiron-humann2:latest -t umigs/chiron-humann2:${IMAGE_VERSION} .
+docker rmi -f chiron-squeezemeta
+docker build --no-cache -t chiron-squeezemeta:latest -t chiron-squeezemeta:${IMAGE_VERSION} .
 docker images
 
 echo "If ready for release, run: "
-echo "  docker push umigs/chiron-humann2:latest"
-echo "  docker push umigs/chiron-humann2:${IMAGE_VERSION}"
+echo "  docker push umigs/chiron-squeezemeta:latest"
+echo "  docker push umigs/chiron-squeezemeta:${IMAGE_VERSION}"
 
